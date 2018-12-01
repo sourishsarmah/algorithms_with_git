40924e08058cf76611930b5eceab466388e988d1
Sourish Sarmah <sourishsarmah@gmail.com>
2018-12-02 02:01:04 +0530

    Added main function to dijkstra.py

--- algorithms/dijkstra/dijkstra.py
+++ algorithms/dijkstra/dijkstra.py
@@ -59,3 +59,23 @@ def dijkstra(graph, source, dest):
     path.reverse()
 
     return path, distances[dest]
+
+def main():
+    count = int(input("Enter no of vertices = "))
+    g = Graph(count)
+
+    edgeCount = int(input("Enter no of edges = "))
+    for i in range(0, edgeCount):
+        src, dest, weight = map(int, input().split())
+        g.addEdge(src, dest, weight)
+
+    src = int(input("Enter source node = "))
+    dest = int(input("Enter destination node = "))
+    shortest_path, distance = dijkstra(g, src, dest)
+    print("Shortest Path : ")
+    print(shortest_path)
+    print(f"Shortest Distance = {distance}")
+
+
+if __name__ == "__main__":
+    main()
